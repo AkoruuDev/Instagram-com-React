c383a68fba0dc68471f8717718d26c87a15eed4e
Akoruu.Dev <akoruu.dev@gmail.com>
2022-07-14 16:08:22 -0300

    BAS [React connection]: Connect js file to render on html file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            <h1>Hey Everyone!</h1>
+            <p>I'm trying make function the Git</p>
+            <p>Let's code with me?</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
