93e81a1933f3dd0501723b2ee897d3cbf69cc4d8
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-14 20:24:42 -0300

    FEAT [Edit User Perfil]: Create a function for change the username and nickname from User

--- src/services/user-edit.js
+++ src/services/user-edit.js
@@ -1,14 +1,21 @@
 export default function UserEdit({setNick, nick, setMyuser, myuser, setShowEdit}) {
+    let username = "";
+    let nickname = "";
     return (
         <div className="user-edit-box">
             <h1 className="edit-title-box">Editar perfil</h1>
             <h3 className="edit-title">Novo nome</h3>
-            <input type={'text'} placeholder="Nome" className="edit-input" />
+            <input type={'text'} placeholder="Nome" className="edit-input" onChange={e => (username = e.target.value)} />
             <h3 className="edit-title">Novo Nickname</h3>
-            <input type={'text'} placeholder="@nickname" className="edit-input" />
-            <div className="button" onClick={() => {
-                setShowEdit(false);
-            }}>Salvar</div>
+            <input type={'text'} placeholder="@nickname" className="edit-input" onChange={e => (nickname = e.target.value)} />
+            <div className="buttons">
+                <div className="button" onClick={() => {
+                    setShowEdit(false);
+                    setMyuser(nickname);
+                    setNick(username);
+                }}>Salvar</div>
+                <div className="button" onClick={() => setShowEdit(false)}>Cancelar</div>
+            </div>
         </div>
     )
 }
